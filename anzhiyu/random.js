4fe1cef8ca764177999a14766ca120c3c28683bb
zentalpha <13231438196@163.com>
2025-12-14 13:59:19 +0800

    Site updated: 2025-12-14 13:59:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/12/12/hello-world/","2025/12/13/关于搭建这个网站/"];function toRandomPost(){
+var posts=["2025/12/13/关于搭建这个网站/","2025/12/12/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
