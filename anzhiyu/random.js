35f3e41a7548867bd97653ddabb42e2ca668f4ad
zentalpha <13231438196@163.com>
2025-12-22 19:00:33 +0800

    Site updated: 2025-12-22 19:00:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/12/22/你是如何访问到这个网站的/"];function toRandomPost(){
+var posts=["2025/12/22/C语言分享/","2025/12/22/你是如何访问到这个网站的/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
